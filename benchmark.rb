f88f6e10e5af0e0cb05a8074e94946247ab2bbcd
dineshgowda24 <dinesh240894@gmail.com>
2022-12-26 14:53:53 +0400

    feat: add benchmark

---
+++ benchmark.rb
@@ -0,0 +1,53 @@
+require 'benchmark'
+require 'faker'
+require_relative 'lib/bitcask'
+
+include Benchmark    
+ 
+disk_store = Bitcask::DiskStore.new('bitcask_benchmark.db')
+value = Faker::Lorem.sentence(word_count: 10)
+
+puts "Benchmark for value size: #{value.length} bytes"
+
+Benchmark.benchmark(CAPTION, 50, FORMAT, "avg_put:", "avg_get:") do |benchmark|
+  tt_put_10k = benchmark.report("DiskStore#put : 10k records") do
+    10_000.times do |n_time|
+        disk_store.put("10_000" + n_time.to_s, value)
+    end
+  end
+
+  tt_get_10k = benchmark.report("DiskStore#get : 10k records") do
+    10_000.times do
+        disk_store.get("10_000" + rand(1..10_000).to_s)
+    end
+  end
+
+  tt_put_100k = benchmark.report("DiskStore#put : 100k records") do
+    100_000.times do |n_time|
+        disk_store.put("100_000" + n_time.to_s, value)
+    end
+  end
+
+  tt_get_100k = benchmark.report("DiskStore#get : 100k records") do
+    100_000.times do
+        disk_store.get("100_000" + rand(1..100_000).to_s)
+    end
+  end
+
+  tt_put_1M = benchmark.report("DiskStore#put : 1M records") do
+    1_000_000.times do |n_time|
+        disk_store.put("1_000_000" + n_time.to_s, value)
+    end
+  end
+
+  tt_get_1M = benchmark.report("DiskStore#get : 1M records") do
+    1_000_000.times do
+        disk_store.get("1_000_000" + rand(1..1_000_000).to_s)
+    end
+  end
+
+  [(tt_put_10k+tt_put_100k+tt_put_1M)/(10_000+100_000+1_000_000).to_f, (tt_get_10k+tt_get_100k+tt_get_1M)/(10_000+100_000+1_000_000).to_f]
+end
+
+File.delete('bitcask_benchmark.db')
+
