0011e749ac5fffa42b56fa6a6bf692d5e04ba92d
dineshgowda24 <dinesh240894@gmail.com>
2022-12-26 22:42:10 +0400

    docs: add missing docs

--- benchmark.rb
+++ benchmark.rb
@@ -9,40 +9,40 @@ include Benchmark
 disk_store = Bitcask::DiskStore.new('bitcask_benchmark.db')
 value = Faker::Lorem.sentence(word_count: 10)
 
-puts "Benchmark for value size: #{value.length} bytes"
+puts "Benchmarked with value_size of #{value.length} bytes"
 
 Benchmark.benchmark(CAPTION, 50, FORMAT, 'avg_put:', 'avg_get:') do |benchmark|
-  tt_put_10k = benchmark.report('DiskStore#put : 10k records') {
+  tt_put_10k = benchmark.report('DiskStore.put : 10k records') {
     10_000.times do |n_time|
       disk_store.put("10_000#{n_time}", value)
     end
   }
 
-  tt_get_10k = benchmark.report('DiskStore#get : 10k records') {
+  tt_get_10k = benchmark.report('DiskStore.get : 10k records') {
     10_000.times do
       disk_store.get("10_000#{rand(1..10_000)}")
     end
   }
 
-  tt_put_100k = benchmark.report('DiskStore#put : 100k records') {
+  tt_put_100k = benchmark.report('DiskStore.put : 100k records') {
     100_000.times do |n_time|
       disk_store.put("100_000#{n_time}", value)
     end
   }
 
-  tt_get_100k = benchmark.report('DiskStore#get : 100k records') {
+  tt_get_100k = benchmark.report('DiskStore.get : 100k records') {
     100_000.times do
       disk_store.get("100_000#{rand(1..100_000)}")
     end
   }
 
-  tt_put_1M = benchmark.report('DiskStore#put : 1M records') {
+  tt_put_1M = benchmark.report('DiskStore.put : 1M records') {
     1_000_000.times do |n_time|
       disk_store.put("1_000_000#{n_time}", value)
     end
   }
 
-  tt_get_1M = benchmark.report('DiskStore#get : 1M records') {
+  tt_get_1M = benchmark.report('DiskStore.get : 1M records') {
     1_000_000.times do
       disk_store.get("1_000_000#{rand(1..1_000_000)}")
     end
