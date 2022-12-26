9f4e4bf4bf56b1ce399045c85fa4dc0c247cf8b6
Dinesh Gowda <dinesh240894@gmail.com>
2022-12-26 20:30:55 +0400

    Feature/int float types (#4)
    * feat: support for int and float types
    
    * feat: support for int and float types
    
    * chores: `rubocop -a`
    
    * chores: `rubocop -A`
    
    * docs: update readme

--- lib/bitcask/serializer.rb
+++ lib/bitcask/serializer.rb
@@ -4,19 +4,42 @@ require 'zlib'
 
 module Bitcask
   module Serializer
-    # tz, keysz, valuesz
-    # 3 - 32 bit unsigned long int with little endian byte order
-    # 3 - 32 bit unsigned long int = 12 bytes
-    HEADER_FORMAT = 'L<L<L<'
-    HEADER_SIZE = 12
+    # tz, keysz, valuesz, keytype, valuetype
+    # 3, 2 - 32, 16 bit unsigned long int with little endian byte order
+    HEADER_FORMAT = 'L<L<L<S<S<'
+    HEADER_SIZE = 16
 
     # 32 bit unsigned long int
     CRC32_FORMAT = 'L'
     CRC32_SIZE = 4
 
+    TYPES = {
+      'Integer' => 1,
+      'Float' => 2,
+      'String' => 3
+    }.freeze
+
+    TYPES_LOOK_UP = {
+      TYPES['Integer'] => 'Integer',
+      TYPES['Float'] => 'Float',
+      TYPES['String'] => 'String'
+    }.freeze
+
+    TYPES_FORMAT = {
+      TYPES['Integer'] => 'q<', # 64 bit signed long int
+      TYPES['Float'] => 'E' # 64 bit double
+    }.freeze
+
     def serialize(epoc:, key:, value:)
-      header = serialize_header(epoc:, keysz: key.length, valuesz: value.length)
-      data = key.encode('utf-8') + value.encode('utf-8')
+      key_type = key.class.to_s
+      value_type = value.class.to_s
+
+      key_raw = pack(key, key_type)
+      value_raw = pack(value, value_type)
+
+      header = serialize_header(epoc: epoc, keysz: key_raw.length, key_type: key_type, value_type: value_type,
+                                valuesz: value_raw.length)
+      data = key_raw + value_raw
 
       [crc32_header_offset + data.length, crc32(header + data) + header + data]
     end
@@ -27,23 +50,29 @@ module Bitcask
 
       return 0, '', '' unless validate_crc32(desearlize_crc32(crc_data), raw_data)
 
-      header_data = data[crc32_offset..header_offset - 1]
-      epoc, keysz, valuesz = deserialize_header(header_data)
+      header_data = data[crc32_offset..crc32_header_offset - 1]
+      epoc, keysz, valuesz, key_type, value_type = deserialize_header(header_data)
 
-      key = data[crc32_header_offset..crc32_header_offset + keysz - 1]
-      value = data[crc32_header_offset + keysz..]
+      key_type = TYPES_LOOK_UP[key_type]
+      value_type = TYPES_LOOK_UP[value_type]
+
+      key_raw = data[crc32_header_offset..crc32_header_offset + keysz - 1]
+      value_raw = data[crc32_header_offset + keysz..]
+
+      key = unpack(key_raw, key_type)
+      value = unpack(value_raw, value_type)
 
       [epoc, key, value]
     end
 
-    def serialize_header(epoc:, keysz:, valuesz:)
-      [epoc, keysz, valuesz].pack(HEADER_FORMAT)
+    def serialize_header(epoc:, key_type:, keysz:, value_type:, valuesz:)
+      [epoc, keysz, valuesz, TYPES[key_type], TYPES[value_type]].pack(HEADER_FORMAT)
     end
 
     def deserialize_header(header_data)
       header = header_data.unpack(HEADER_FORMAT)
 
-      [header[0], header[1], header[2]]
+      [header[0], header[1], header[2], header[3], header[4]]
     end
 
     def crc32_offset
@@ -68,6 +97,28 @@ module Bitcask
 
     def validate_crc32(digest, data)
       digest == Zlib.crc32(data)
+    end
+
+    def pack(attribute, attribute_type)
+      case attribute_type
+      when 'Integer', 'Float'
+        [attribute].pack(TYPES_FORMAT[TYPES[attribute_type]])
+      when 'String'
+        attribute.encode('utf-8')
+      else
+        raise StandardError, 'Invalid attribute_type'
+      end
+    end
+
+    def unpack(attribute, attribute_type)
+      case attribute_type
+      when 'Integer', 'Float'
+        attribute.unpack1(TYPES_FORMAT[TYPES[attribute_type]])
+      when 'String'
+        attribute
+      else
+        raise StandardError, 'Invalid attribute_type'
+      end
     end
   end
 end
--- spec/bitcask/serializer_spec.rb
+++ spec/bitcask/serializer_spec.rb
@@ -2,17 +2,17 @@
 
 RSpec.describe Bitcask::Serializer do
   let(:crc_size) { 4 }
-  let(:header_size) { 12 }
-  let(:crc_and_header_size) { 16 }
+  let(:header_size) { 16 }
+  let(:crc_and_header_size) { header_size + crc_size }
   let(:subject) { Class.new { extend Bitcask::Serializer } }
   let(:now) { Time.now.to_i }
 
   describe '#serialize' do
     let(:key) { Faker::Lorem.word }
-    let(:value) { Faker::Lorem.sentence(word_count: 10000) }
+    let(:value) { Faker::Lorem.sentence(word_count: 10_000) }
 
     it 'returns serialized data and its size' do
-      size, data = subject.serialize(epoc: now, key:, value:)
+      size, data = subject.serialize(epoc: now, key: key, value: value)
 
       expect(size).to eq(crc_and_header_size + key.length + value.length)
       expect(data).not_to be_empty
@@ -21,7 +21,7 @@ RSpec.describe Bitcask::Serializer do
 
   describe '#serialize_header' do
     it 'returns serialized header' do
-      data = subject.serialize_header(epoc: now, keysz: 10, valuesz: 20)
+      data = subject.serialize_header(epoc: now, keysz: 10, valuesz: 20, key_type: 'Integer', value_type: 'Integer')
 
       expect(data.length).to eq(header_size)
       expect(data).not_to be_empty
@@ -29,18 +29,42 @@ RSpec.describe Bitcask::Serializer do
   end
 
   describe '#deserialize' do
-    let(:serialized_data) do
-      OpenStruct.new(raw: "\xCEi\x94\x03}\xA7\xA8c\x05\x00\x00\x00\t\x00\x00\x00animeOne Piece",
-                     epoc: 1_671_997_309, key: 'anime', value: 'One Piece')
+    let(:serialized_data_1) do
+      OpenStruct.new(raw: "<\v\x90\x8C\x01\xC1\xA9c\b\x00\x00\x00\b\x00\x00\x00\x01\x00\x02\x00\x01\x00\x00\x00\x00\x00\x00\x00333333\xF3?",
+                     epoc: 1_672_069_377, key: 1, value: 1.2)
+    end
+    let(:serialized_data_2) do
+      OpenStruct.new(raw: "\xFE\xE9\xD2.\x01\xC1\xA9c\b\x00\x00\x00\b\x00\x00\x00\x02\x00\x01\x00333333\xF3?\x02\x00\x00\x00\x00\x00\x00\x00",
+                     epoc: 1_672_069_377, key: 1.2, value: 2)
+    end
+    let(:serialized_data_3) do
+      OpenStruct.new(raw: "\xBA\xFD\x0EA\x01\xC1\xA9c\x05\x00\x00\x00\t\x00\x00\x00\x03\x00\x03\x00AnimeOne Piece",
+                     epoc: 1_672_069_377, key: 'Anime', value: 'One Piece')
     end
 
     context 'when a valid data is passed' do
       it 'returns epoc, key and value' do
-        epoc, key, value = subject.deserialize(serialized_data.raw)
+        epoc, key, value = subject.deserialize(serialized_data_1.raw)
 
-        expect(epoc).to eq(serialized_data.epoc)
-        expect(key).to eq(serialized_data.key)
-        expect(value).to eq(serialized_data.value)
+        expect(epoc).to eq(serialized_data_1.epoc)
+        expect(key).to eq(serialized_data_1.key)
+        expect(value).to eq(serialized_data_1.value)
+      end
+
+      it 'returns epoc, key and value' do
+        epoc, key, value = subject.deserialize(serialized_data_2.raw)
+
+        expect(epoc).to eq(serialized_data_2.epoc)
+        expect(key).to eq(serialized_data_2.key)
+        expect(value).to eq(serialized_data_2.value)
+      end
+
+      it 'returns epoc, key and value' do
+        epoc, key, value = subject.deserialize(serialized_data_3.raw)
+
+        expect(epoc).to eq(serialized_data_3.epoc)
+        expect(key).to eq(serialized_data_3.key)
+        expect(value).to eq(serialized_data_3.value)
       end
     end
 
@@ -69,16 +93,18 @@ RSpec.describe Bitcask::Serializer do
     let(:key) { 'anime' }
     let(:value) { 'One Piece' }
     let(:serialized_header_data) do
-      OpenStruct.new(raw: "\x1F\xAB\xA8c\n\x00\x00\x00\x14\x00\x00\x00", epoc: 1_671_998_239, keysz: 10,
-                     valuesz: 20)
+      OpenStruct.new(raw: "\xF5\xC1\xA9c\n\x00\x00\x00\x14\x00\x00\x00\x01\x00\x01\x00", epoc: 1_672_069_621, keysz: 10,
+                     valuesz: 20, value_type: 1, key_type: 1)
     end
 
     it 'returns epoc, keysz and valuesz' do
-      epoc, keysz, valuesz = subject.deserialize_header(serialized_header_data.raw)
+      epoc, keysz, valuesz, key_type, value_type = subject.deserialize_header(serialized_header_data.raw)
 
       expect(epoc).to eq(serialized_header_data.epoc)
       expect(keysz).to eq(serialized_header_data.keysz)
       expect(valuesz).to eq(serialized_header_data.valuesz)
+      expect(key_type).to eq(serialized_header_data.key_type)
+      expect(value_type).to eq(serialized_header_data.value_type)
     end
   end
 end
